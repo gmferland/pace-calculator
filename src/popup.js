3371bdb5730f6937b43af60acec070c01fa8d260
gmferland <gmferland@gmail.com>
2018-04-07 13:09:32 -0400

    basic select and input

--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,46 @@
-let changeColor = document.getElementById('changeColor');
+const events = [
+  {
+    name: '<script type="text/javascript></script>',
+    distance: 1609,
+  },
+  {
+    name: '5k',
+    distance: 5000,
+  },
+  {
+    name: '10k',
+    distance: 10000,
+  },
+];
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+const selectEvent = document.getElementById('event');
+events.forEach(function(e) {
+  const child = document.createElement('option');
+  child.setAttribute('value', escapeText(e.name));
+  child.innerHTML = escapeText(e.name);
+  selectEvent.appendChild(child);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  console.log('onclick');
-  chrome.tabs.executeScript(null, {
-    code: 'document.body.style.backgroundColor = "' + color + '";',
-  });
-};
+function escapeText(text) {
+  let returnText = '';
+  for (let i = 0; i < text.length; i++) {
+    const char = text.charAt(i);
+    switch (char) {
+      case '&':
+        returnText = returnText.concat('&amp');
+      case '<':
+        returnText = returnText.concat('&lt');
+      case '>':
+        returnText = returnText.concat('&gt');
+      case '"':
+        returnText = returnText.concat('&quot');
+      case "'":
+        returnText = returnText.concat('&#x27');
+      case '/':
+        returnText = returnText.concat('&#x2F');
+      default:
+        returnText = returnText.concat(char);
+    }
+  }
+  return returnText;
+}
