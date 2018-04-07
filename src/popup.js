a81a0f589588324dba3885c48cdbe62e969957e4
gmferland <gmferland@gmail.com>
2018-04-07 13:25:05 -0400

    I don't think there's a need to escape

--- src/popup.js
+++ src/popup.js
@@ -16,31 +16,7 @@ const events = [
 const selectEvent = document.getElementById('event');
 events.forEach(function(e) {
   const child = document.createElement('option');
-  child.setAttribute('value', escapeText(e.name));
-  child.innerHTML = escapeText(e.name);
+  child.setAttribute('value', e.name);
+  child.innerHTML = e.name;
   selectEvent.appendChild(child);
 });
-
-function escapeText(text) {
-  let returnText = '';
-  for (let i = 0; i < text.length; i++) {
-    const char = text.charAt(i);
-    switch (char) {
-      case '&':
-        returnText = returnText.concat('&amp');
-      case '<':
-        returnText = returnText.concat('&lt');
-      case '>':
-        returnText = returnText.concat('&gt');
-      case '"':
-        returnText = returnText.concat('&quot');
-      case "'":
-        returnText = returnText.concat('&#x27');
-      case '/':
-        returnText = returnText.concat('&#x2F');
-      default:
-        returnText = returnText.concat(char);
-    }
-  }
-  return returnText;
-}
