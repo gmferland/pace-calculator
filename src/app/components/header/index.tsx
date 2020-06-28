3baf1be8b629f23f981be1bd1dd21cf5c6ed391f
Greg Ferland <gmferland@MacBook-Pro.local>
2020-06-28 13:12:41 -0400

    mobile menu button

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,20 +1,25 @@
 import { FunctionalComponent, h } from 'preact';
 import { Link } from 'preact-router/match';
+import HamburgerMenu from './HamburgerMenu';
 import * as style from './style.css';
 import * as global from 'app/style/global.css';
 
 const Header: FunctionalComponent = () => {
   return (
     <header class={style.header}>
-      <div class={global.container}>
+      <div class={`${global.container} ${style.headerContainer}`}>
         <nav class={style.nav}>
-          <Link href="/">
-            <h1 class={style.title}>Pace Calculator</h1>
-          </Link>
-          <div class={style.links}>
+          <h1 class={style.title}>Race Pace</h1>
+          <div class={`${style.links} ${global.mobileHidden}`}>
+            <Link activeClassName={style.active} href="/">
+              Pace Calculator
+            </Link>
             <Link activeClassName={style.active} href="/race-predictor">
               Race Predictor
             </Link>
+          </div>
+          <div class={global.mobileOnly}>
+            <HamburgerMenu></HamburgerMenu>
           </div>
         </nav>
       </div>
